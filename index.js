3cfee2b34a3cb08bffa5e9ef3aa50cbc367e656d
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-11 01:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,3 +31,8 @@ function removeLastDriver() {
   const newDrivers = drivers.slice(0, drivers.length - 1);
   return newDrivers;
 }
+
+function removeFirstDriver() {
+  const newDrivers = drivers.slice(1);
+  return newDrivers;
+}
